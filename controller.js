edd355c8735357ad55fc801fe0f65c6a23e0c91a
Rizfa12 <158300942+Rizfa12@users.noreply.github.com>
2024-02-22 11:36:28 +0700

    semua file

---
+++ controller.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var response = require('./res');
+var connection = require('./koneksi');
---
+++ koneksi.js
@@ -0,0 +1,14 @@
+var mysql = required('mysql');
+
+const conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'moch'
+});
+conn.connect((err)=>{
+    if(err) throw err;
+    console.log('Mysql terkoneksi');
+});
+
+module.exports = conn; 
